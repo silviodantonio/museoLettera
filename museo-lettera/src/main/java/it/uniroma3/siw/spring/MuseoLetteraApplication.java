d7338444c9ae0ff5af0dd387f5609656865fad68
silviodantonio <silviodantonio.99@gmail.com>
2021-05-24 19:03:51 +0200

    Creato progetto spring e inizializzato
    Spostate le pagine html e relativi fogli di stile

---
+++ museo-lettera/src/main/java/it/uniroma3/siw/spring/MuseoLetteraApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseoLetteraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseoLetteraApplication.class, args);
+	}
+
+}
---
+++ museo-lettera/src/test/java/it/uniroma3/siw/spring/MuseoLetteraApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseoLetteraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
